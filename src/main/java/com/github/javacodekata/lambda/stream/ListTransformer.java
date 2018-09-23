c8811d1a74f3e398d0707f06cc725965de800726
German Gonzalez <german_gonzalez@gap.com>
2018-09-23 18:32:15 -0300

    getSortedDescendingIntegers

--- src/main/java/com/github/javacodekata/lambda/stream/ListTransformer.java
+++ src/main/java/com/github/javacodekata/lambda/stream/ListTransformer.java
@@ -2,7 +2,7 @@ package com.github.javacodekata.lambda.stream;
 
 import com.google.common.collect.ImmutableList;
 
-import java.util.LinkedList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -57,6 +57,11 @@ public class ListTransformer {
 	 * @return
 	 */
 	public List<Integer> getSortedDescendingIntegers() {
-		return new LinkedList<>();
+        return values.stream()
+                .filter(word ->
+                                word.chars().allMatch(c -> Character.isDigit(c)))
+                .map(word -> Integer.valueOf(word))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
 	}
 }
