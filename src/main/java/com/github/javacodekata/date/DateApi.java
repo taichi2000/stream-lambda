f2b8fe6fce2a9b3a44a88b22f3a79adbe0b71115
German Gonzalez <german_gonzalez@gap.com>
2018-09-24 22:09:54 -0300

    added Date API test: Days between

---
+++ src/main/java/com/github/javacodekata/date/DateApi.java
@@ -0,0 +1,9 @@
+package com.github.javacodekata.date;
+
+import java.util.Date;
+
+public class DateApi {
+    public long daysBetween(Date origin, Date end) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/github/javacodekata/date/DateApiTest.java
@@ -0,0 +1,34 @@
+package com.github.javacodekata.date;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DateApiTest {
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private DateApi dateApi;
+
+
+    @Before
+    public void setUp() throws Exception {
+        dateApi = new DateApi();
+    }
+
+
+    @Test
+    public void daysBetweenDates() throws Exception {
+        Date origin = dateFormat.parse("2018-09-01 11:00:00");
+        Date end = dateFormat.parse("2018-09-18 19:00:00");
+
+        long daysBetween = dateApi.daysBetween(origin, end);
+
+        assertThat(daysBetween, is(18));
+    }
+}
