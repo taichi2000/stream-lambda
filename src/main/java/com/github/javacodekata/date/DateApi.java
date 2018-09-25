737ada08d13a8daf1912695f578efa078c106df5
German Gonzalez <german_gonzalez@gap.com>
2018-09-24 22:43:33 -0300

    daysBetweenDates

--- src/main/java/com/github/javacodekata/date/DateApi.java
+++ src/main/java/com/github/javacodekata/date/DateApi.java
@@ -1,10 +1,16 @@
 package com.github.javacodekata.date;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class DateApi {
     public long daysBetween(Date origin, Date end) {
-        return 0;
+        LocalDate localOrigin = origin.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate localEnd = end.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        return ChronoUnit.DAYS.between(localOrigin, localEnd);
     }
 
     public DateRange searchRangeSince(Date date, int daysBefore){
